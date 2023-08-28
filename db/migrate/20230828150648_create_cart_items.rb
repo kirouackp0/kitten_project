58bc5a760ffaf9046efae2075a77097eb059402d
greg-Da <gregoiredalmasso@gmail.com>
2023-08-28 17:41:44 +0200

    feat(migr): add cart/cart_item migration

---
+++ db/migrate/20230828150648_create_cart_items.rb
@@ -0,0 +1,12 @@
+class CreateCartItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cart_items do |t|
+      t.belongs_to :cart, index: true
+      t.belongs_to :item, index: true
+      t.integer :quantity
+
+
+      t.timestamps
+    end
+  end
+end
